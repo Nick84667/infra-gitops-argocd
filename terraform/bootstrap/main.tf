8ae445660c55ddcd74d217f65a9aae2d728e8d6f
Nicola Puoti <np84digitalproduct@gmail.com>
2026-02-25 18:23:54 +0100

    Update main.tf

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -25,5 +25,7 @@ resource "aws_dynamodb_table" "lock" {
   name         = "terraform-locks-gitops-argocd"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-  attribute { name = "LockID", type = "S" }
+  attribute {
+  name = "LockID"
+  type = "S"
 }
