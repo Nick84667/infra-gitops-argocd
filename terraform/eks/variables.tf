88f0e27a32b1b7040dbfca1bb794653d9052b63e
Nicola Puoti <np84digitalproduct@gmail.com>
2026-02-26 18:21:28 +0100

    Update variables.tf

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -16,6 +16,15 @@ variable "public_subnet_ids" {
   type = list(string)
 }
 
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "my_ip_cidr" {
+  type        = string
+  description = "CIDR allowed to access EKS public endpoint (e.g. x.x.x.x/32)"
+}
+
 variable "node_instance_type" {
   type    = string
   default = "t3.small"
