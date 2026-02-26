af22ed367cb4312115b35bbe8bcb9867aedb5778
Nicola Puoti <np84digitalproduct@gmail.com>
2026-02-26 14:27:24 +0100

    Update vpc-endpoints.tf

--- terraform/base/vpc-endpoints.tf
+++ terraform/base/vpc-endpoints.tf
@@ -1,1 +1,24 @@
+# --- SG for VPC Interface Endpoints ---
+resource "aws_security_group" "vpce_sg" {
+  name        = "${var.project}-vpce-sg"
+  description = "SG for VPC Interface Endpoints"
+  vpc_id      = aws_vpc.this.id
 
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.this.cidr_block]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "${var.project}-vpce-sg" }
+}
