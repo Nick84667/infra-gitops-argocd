a758e03438b9dd8436d4e908e32bcd3d5ee44e24
Nicola Puoti <np84digitalproduct@gmail.com>
2026-02-26 18:08:17 +0100

    Update outputs.tf

--- terraform/base/outputs.tf
+++ terraform/base/outputs.tf
@@ -5,6 +5,9 @@ output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
 output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
