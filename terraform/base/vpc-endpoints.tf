0582827123ca5d249cc47ee66f22659dd660bbd1
Nicola Puoti <np84digitalproduct@gmail.com>
2026-02-26 14:29:01 +0100

    Update vpc-endpoints.tf

--- terraform/base/vpc-endpoints.tf
+++ terraform/base/vpc-endpoints.tf
@@ -22,3 +22,41 @@ resource "aws_security_group" "vpce_sg" {
 
   tags = { Name = "${var.project}-vpce-sg" }
 }
+
+# --- Gateway Endpoint: S3 (needed for ECR layers, etc.) ---
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [aws_route_table.private.id]
+
+  tags = { Name = "${var.project}-vpce-s3" }
+}
+
+# --- Interface Endpoints (no NAT) ---
+locals {
+  interface_endpoints = toset([
+    "com.amazonaws.${var.region}.eks",
+    "com.amazonaws.${var.region}.ecr.api",
+    "com.amazonaws.${var.region}.ecr.dkr",
+    "com.amazonaws.${var.region}.ec2",
+    "com.amazonaws.${var.region}.sts",
+    "com.amazonaws.${var.region}.logs"
+  ])
+}
+
+resource "aws_vpc_endpoint" "iface" {
+  for_each           = local.interface_endpoints
+  vpc_id             = aws_vpc.this.id
+  service_name       = each.value
+  vpc_endpoint_type  = "Interface"
+
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.vpce_sg.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${var.project}-vpce-${replace(each.value, "com.amazonaws.${var.region}.", "")}"
+  }
+}
