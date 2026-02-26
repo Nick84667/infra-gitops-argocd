3bd0bc7987f66174ef917e947670fcb4dc5b8e8e
Nicola Puoti <np84digitalproduct@gmail.com>
2026-02-26 14:26:51 +0100

    Update network-private.tf

--- terraform/base/network-private.tf
+++ terraform/base/network-private.tf
@@ -1,1 +1,26 @@
+# --- Private subnets (2 AZ) - no NAT route ---
 
+resource "aws_subnet" "private" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.azs.names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name                                         = "${var.project}-private-${count.index}"
+    "kubernetes.io/role/internal-elb"            = "1"
+    "kubernetes.io/cluster/${var.cluster_name}"  = "shared"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+  tags   = { Name = "${var.project}-private-rt" }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
