f167bcb7aa6a6ed6ec3430f333c138a08d653a5e
Nicola Puoti <np84digitalproduct@gmail.com>
2026-03-02 14:30:55 +0100

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -27,8 +27,7 @@ resource "aws_eks_cluster" "this" {
   subnet_ids              = concat(var.public_subnet_ids, var.private_subnet_ids)
   endpoint_public_access  = true
   endpoint_private_access = true
-  public_access_cidrs     = [var.my_ip_cidr]
-}
+  }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
